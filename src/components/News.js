b6942f9bfa3f9956aef9c1263786d6341de03f99
Dharmik Patel <patel93@sheridancollege.ca>
2025-07-09 23:48:10 -0400

    created basic outline for newsitem

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,34 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is the news component
-            <NewsItem/>
-            <NewsItem/>
-            <NewsItem/>
-            <NewsItem/>
-            <NewsItem/>
-            <NewsItem/>
+      <div className='container my-3'>
+    
+        <h2>NewsMonkey top - headline</h2>
+        <div className="row">
+            <div className='col-md-4'>
+                      <NewsItem title = "mytitle"  description = "mydescription"/>
+            </div>
+              <div className='col-md-4'>
+                      <NewsItem title = "mytitle"  description = "mydescription"/>
+            </div>
+              <div className='col-md-4'>
+                      <NewsItem title = "mytitle"  description = "mydescription"/>
+            </div>
+            
+        </div>
+               <div className="row">
+            <div className='col-md-4'>
+                      <NewsItem title = "mytitle"  description = "mydescription"/>
+            </div>
+              <div className='col-md-4'>
+                      <NewsItem title = "mytitle"  description = "mydescription"/>
+            </div>
+              <div className='col-md-4'>
+                      <NewsItem title = "mytitle"  description = "mydescription"/>
+            </div>
+            
+        </div>
+        
         
       </div>
   
